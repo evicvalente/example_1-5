7bc435a0f6ac8a21d969bfcd3bfb47b6764a5a18
Emma Valente <emmavvalente@gmail.com>
2025-01-09 17:31:39 +0000

    system is locked on the 4th try, and buttons B, C along with the B1 USER button. Also, retry is pressing AD at the same time

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    DigitalIn enterButton(BUTTON1);
+    DigitalIn enterButton(BUTTON1);       
     DigitalIn gasDetector(D2);
     DigitalIn overTempDetector(D3);
     DigitalIn aButton(D4);
@@ -37,22 +37,32 @@ int main()
 
         alarmLed = alarmState;
 
-        if ( numberOfIncorrectCodes < 5 ) {
-            
-            if ( aButton && bButton && cButton && dButton && !enterButton ) {
+        // Allow code entry attempts only if the user has made fewer than 3 errors
+        // (the 4th error locks the system)
+        if ( numberOfIncorrectCodes < 3 ) {
+
+            // "Retry" condition: user presses A and D simultaneously (and *not* B, C, or enter)
+            if ( aButton && dButton && !bButton && !cButton && !enterButton ) {
                 incorrectCodeLed = OFF;
             }
 
-            if ( enterButton && !incorrectCodeLed && alarmState) {
-                if ( aButton && bButton && !cButton && !dButton ) {
+            // If the alarm is on, the user can press the enter button (B1) to try the code
+            if ( enterButton && !incorrectCodeLed && alarmState ) {
+
+                // Correct code: Press B and C (while not pressing A or D)
+                if ( bButton && cButton && !aButton && !dButton ) {
+                    // Turn off the alarm and reset the error counter
                     alarmState = OFF;
                     numberOfIncorrectCodes = 0;
                 } else {
+                    // Wrong code -> set the error LED and increment the error counter
                     incorrectCodeLed = ON;
-                    numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
+                    numberOfIncorrectCodes++;
                 }
             }
+
         } else {
+            // Lock the system on the 4th incorrect attempt
             systemBlockedLed = ON;
         }
     }
